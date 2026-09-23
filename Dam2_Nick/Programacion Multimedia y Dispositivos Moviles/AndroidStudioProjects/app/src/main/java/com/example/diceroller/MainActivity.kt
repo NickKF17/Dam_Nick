ffea721251b37103ae7dc7adc06232fc395dd262
NickKF <nickmaverick07072007@gmail.com>
2026-09-23 12:38:48 +0200

    23/09

---
+++ Dam2_Nick/Programacion Multimedia y Dispositivos Moviles/AndroidStudioProjects/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+            rollDice()/*  Texto temporal para saber que funciona el boton
+                 val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+                 toast.show()
+                 //otra forma de ponerlo
+                 //Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+             */
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+
+    }
+
+}
+
+class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
